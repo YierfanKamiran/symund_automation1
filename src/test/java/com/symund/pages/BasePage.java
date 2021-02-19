b4b59cb075ccb7ec29dd2609f6592814edadb000
Zebo Narimanova <znarimanova11@gmail.com>
2021-02-19 12:19:02 +0000

    Base Page update

--- src/test/java/com/symund/pages/BasePage.java
+++ src/test/java/com/symund/pages/BasePage.java
@@ -1,9 +1,4 @@
 package com.symund.pages;
 
-import com.symund.utilities.Driver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-
 public abstract class BasePage {
 }
