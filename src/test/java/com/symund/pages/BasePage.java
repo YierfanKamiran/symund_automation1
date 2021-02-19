6971af1be66198fa9c4a9e5760283502990beef1
Zebo Narimanova <znarimanova11@gmail.com>
2021-02-19 12:15:22 +0000

    BasePage updated in my branch

--- src/test/java/com/symund/pages/BasePage.java
+++ src/test/java/com/symund/pages/BasePage.java
@@ -6,7 +6,4 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public abstract class BasePage {
-    public BasePage() {
-        PageFactory.initElements(Driver.get(), this);
-    }
 }
