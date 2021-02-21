015eeda6358eb79de4b497d571bdf55feff107b2
evrimeylem <evrimeylemo@gmail.com>
2021-02-21 15:30:15 +0000

    profile setting is started

--- src/test/java/com/symund/pages/profileSettingPage.java
+++ src/test/java/com/symund/pages/profileSettingPage.java
@@ -2,11 +2,21 @@ package com.symund.pages;
 
 
 import com.symund.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
 
 public class profileSettingPage extends BasePage {
     public profileSettingPage() {
 
-        PageFactory.initElements(Driver.get(), this);
-    }
+        PageFactory.initElements(Driver.get(), this); }
+
+
+    @FindBy(css = ".avatardiv.avatardiv-shown")
+    public WebElement profileicon;
+
+
+
+
 }
