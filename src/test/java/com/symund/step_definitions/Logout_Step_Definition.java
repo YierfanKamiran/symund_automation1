e97b441c544d5541be0cf9867010c00a540af652
Zebo Narimanova <znarimanova11@gmail.com>
2021-02-23 12:28:58 +0000

    SYM-138

--- src/test/java/com/symund/step_definitions/Logout_Step_Definition.java
+++ src/test/java/com/symund/step_definitions/Logout_Step_Definition.java
@@ -16,17 +16,15 @@ public class Logout_Step_Definition {
     @Given("User is on the login page and page title is <Symund - QA>")
     public void user_is_on_the_login_page_and_page_title_is_Symund_QA() {
         Driver.get().get("https://qa.symund.com");
-        BrowserUtils.waitFor(3);
-
     }
 
     @When("User login to symund website successfully")
     public void user_login_to_symund_website_successfully() {
-        logoutPage.username.sendKeys(ConfigurationReader.get("username2"));
-        logoutPage.password.sendKeys(ConfigurationReader.get("password2"));
+        logoutPage.username.sendKeys(ConfigurationReader.get("username1"));
+        logoutPage.password.sendKeys(ConfigurationReader.get("password1"));
+        BrowserUtils.waitFor(2);
         logoutPage.login.click();
     }
-
     @Then("User Able to click on Logout avatar")
     public void user_Able_to_click_on_Logout_avatar() {
         BrowserUtils.waitForVisibility(logoutPage.avatar,3);
