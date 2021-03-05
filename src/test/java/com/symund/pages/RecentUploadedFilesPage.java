9e26786c6f50a75e38b6a69fdae5d36fb0b0dfc5
evrimeylem <evrimeylemo@gmail.com>
2021-03-05 00:10:15 +0000

    started locating my work

--- src/test/java/com/symund/pages/RecentUploadedFilesPage.java
+++ src/test/java/com/symund/pages/RecentUploadedFilesPage.java
@@ -16,12 +16,9 @@ public class RecentUploadedFilesPage extends BasePage {
     @FindBy(xpath = "//*[@id='view13-input-folder']")
     public WebElement folderInputbox;
 
-    //*[@id="controls"]/div[2]/div[2]/ul/li[2]/a/form/input[2]
 
 
 
-        @FindBy(xpath = "//span[.='file']")
-        public WebElement fileName;
 
     }
 
