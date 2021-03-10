1399be0f63f364b28b1def75681cdb0fa833b204
YierfanKamiran <YierfanKamiran@users.noreply.github.com>
2021-03-10 20:25:42 +0300

    The user should should be able to comment

--- src/test/resources/features/Comment.feature
+++ src/test/resources/features/Comment.feature
@@ -3,9 +3,19 @@ Feature: Comment Box Functions
   Background:
     Given the user is on the login page
     And the user enters "Employee23" and "Employee123"
+    Given the user clicks dots for comments
+    And the user clicks Details
+    And the user clicks comments
 @comment
   Scenario: A comment box should be displayed
     Given the user clicks dots for comments
     And the user clicks Details
     And the user clicks comments
     Then the comments box should be displayed
+
+  @comment
+  Scenario: the user should be able to comment
+    Given the user enters comment "I love you "
+    And the user clicks comment arrow button
+    And the user's username should be seen
+    And the box is emptied for new comment
--- target/default-html-reports/report.js
+++ target/default-html-reports/report.js
@@ -32,6 +32,36 @@ formatter.match({
 formatter.result({
   "status": "passed"
 });
+formatter.step({
+  "name": "the user clicks dots for comments",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.symund.step_definitions.CommentStepDefs.theUserClicksDotsForComments()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user clicks Details",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.symund.step_definitions.DetailsFileFolder_StepDefs.the_user_clicks_Details()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user clicks comments",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.symund.step_definitions.CommentStepDefs.theUserClicksComments()"
+});
+formatter.result({
+  "status": "passed"
+});
 formatter.scenario({
   "name": "A comment box should be displayed",
   "description": "",
@@ -78,6 +108,117 @@ formatter.step({
 });
 formatter.match({
   "location": "com.symund.step_definitions.CommentStepDefs.theCommentsBoxShouldBeDisplayed()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user is on the login page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.symund.step_definitions.LoginStepDefs.the_user_is_on_the_login_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user enters \"Employee23\" and \"Employee123\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.symund.step_definitions.LoginStepDefs.the_user_enters_and(java.lang.String,java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user clicks dots for comments",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.symund.step_definitions.CommentStepDefs.theUserClicksDotsForComments()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user clicks Details",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.symund.step_definitions.DetailsFileFolder_StepDefs.the_user_clicks_Details()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user clicks comments",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.symund.step_definitions.CommentStepDefs.theUserClicksComments()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "the user should be able to comment",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@comment"
+    }
+  ]
+});
+formatter.step({
+  "name": "the user enters comment \"I love you \"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.symund.step_definitions.CommentStepDefs.theUserEntersComment(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user clicks comment arrow button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.symund.step_definitions.CommentStepDefs.theUserClicksCommentArrowButton()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user\u0027s username should be seen",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.symund.step_definitions.CommentStepDefs.theUserSUsernameShouldBeSeen()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the box is emptied for new comment",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.symund.step_definitions.CommentStepDefs.theBoxIsEmptiedForNewComment()"
 });
 formatter.result({
   "status": "passed"
