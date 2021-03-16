312ab3ef1b3f80b368f6660bc845ae6b54147bcb
YierfanKamiran <YierfanKamiran@users.noreply.github.com>
2021-03-16 08:10:22 +0300

    All work is done

--- src/test/java/com/symund/runners/CukesRunner.java
+++ src/test/java/com/symund/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         glue = "com/symund/step_definitions",
         dryRun = false,
         tags = "@comment"
-        //l
+        ////
 )
 public class CukesRunner {
 }
